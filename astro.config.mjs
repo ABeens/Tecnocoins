c8c8169efc74ad2bcfefadb0b6a1de929916fb8a
Abraham <41970794+ABeens@users.noreply.github.com>
2025-08-17 15:27:54 -0600

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,10 +6,7 @@ import tailwindcss from '@tailwindcss/vite';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://abeens.github.io/Tecnocoins/',
-  trailingSlash: 'always',
-  build: {
-    format: 'directory'
-  },
+  base:'.',
   vite: {
     plugins: [tailwindcss()]
   }
